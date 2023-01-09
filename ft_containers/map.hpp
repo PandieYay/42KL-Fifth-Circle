7470ea4117a05925238c704b1499337358f06535
PandieYay <babyedison@gmail.com>
2023-01-09 16:08:57 +0800

    map W.I.P, default constructor added

--- ft_containers/map.hpp
+++ ft_containers/map.hpp
@@ -3,13 +3,65 @@
 
 #include <memory>
 #include <functional>
+#include "functions.hpp"
 
 namespace ft
 {
     template <class Key, class T,
               class Compare = std::less<Key>,
-              class Allocator = std::allocator<std::pair<const Key, T> > >
-    class map;
+              class Allocator = std::allocator<ft::pair<const Key, T> > >
+    class map
+    {
+    public:
+        typedef pair<const Key, T> value_type;
+        typedef std::less<Key> key_compare;
+        typedef Allocator allocator_type;
+        typedef size_t size_type;
+
+    private:
+        value_type *_vector;
+        key_compare _key;
+        Allocator _alloc;
+        size_type _size;
+
+    public:
+        // Constructors
+        /// @brief Constructs an empty container, with no elements.
+        explicit map(const key_compare &comp = key_compare(), const allocator_type &alloc = allocator_type()) : _key(comp), _alloc(alloc)
+        {
+            _vector = NULL;
+        }
+        /// @brief Constructs a container with as many elements as the range [first,last),
+        /// with each element constructed from its corresponding element in that range.
+        template <class InputIterator>
+        map(typename ft::enable_if<!ft::is_integral<InputIterator>::value, InputIterator>::type first, InputIterator last,
+            const key_compare &comp = key_compare(), const allocator_type &alloc = allocator_type()) : _key(comp), _alloc(alloc)
+        {
+            size_type size = last - first;
+            _vector = _alloc.allocate(size);
+            for (size_type i = 0; i < size; i++)
+            {
+                _alloc.construct(_vector + i, *first);
+                first++;
+            }
+            _size = size;
+        }
+        /// @brief Copies all the elements from x into the container.
+        map &operator=(const map &x)
+        {
+            for (size_type i = 0; i < _size; i++)
+                _alloc.destroy(_vector + i);
+            _alloc.deallocate(_vector, _size);
+            _vector = _alloc.allocate(x._size);
+            _size = 0;
+            while (_size < x._size)
+            {
+                _alloc.construct(_vector + _size, x._vector[_size]);
+                _size++;
+            }
+            return *this;
+        }
+    };
 }
 
 #endif
---
+++ ft_containers/testers/maptester.cpp
@@ -0,0 +1,19 @@
+#include "../map.hpp"
+#include <map>
+#include <string>
+#include <iostream>
+
+using std::cout;
+
+int main(void)
+{
+    ft::map<char, int> first;
+
+    // first['a'] = 10;
+    // first['b'] = 30;
+    // first['c'] = 50;
+    // first['d'] = 70;
+
+    // ft::map<char, int> second(first.begin(), first.end());
+    // cout << second['a'];
+}

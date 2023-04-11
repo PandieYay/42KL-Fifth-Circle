d696925482b5019e32dc563ef4f51837b6543b40
PandieYay <babyedison@gmail.com>
2023-04-11 12:14:56 +0800

    Made php work with server

--- Inception/wordpress/wp-setup.sh
+++ Inception/wordpress/wp-setup.sh
@@ -5,5 +5,7 @@ chown -R www-data:www-data /var/www/html/wordpress/
 chmod -R 755 /var/www/html/wordpress/
 mkdir /var/www/html/wordpress/wp-content/uploads
 chown -R www-data:www-data /var/www/html/wordpress/wp-content/uploads/
+echo "<?php echo phpinfo();?>" > /var/www/html/info.php
 echo "Wordpress sh script done"
-sleep 5
+mkdir -p /run/php
+/usr/sbin/php-fpm7.3 -F -R

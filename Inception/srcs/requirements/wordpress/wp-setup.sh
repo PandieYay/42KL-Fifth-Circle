1ecdfb394b1ed8e9f978193cdd03ac2a88de0982
PandieYay <babyedison@gmail.com>
2023-04-24 18:35:17 +0800

    Redis now works with wordpress

--- Inception/srcs/requirements/wordpress/wp-setup.sh
+++ Inception/srcs/requirements/wordpress/wp-setup.sh
@@ -1,12 +1,29 @@
 cd tmp
-mv wordpress /var/www/html/
-mv index.html /var/www/html/
-mv wp-config.php /var/www/html/wordpress
-chown -R www-data:www-data /var/www/html/wordpress/
-chmod -R 755 /var/www/html/wordpress/
-mkdir /var/www/html/wordpress/wp-content/uploads
-chown -R www-data:www-data /var/www/html/wordpress/wp-content/uploads/
+mv wordpress/* /var/www/html/
+mv wp-config.php /var/www/html
+chown -R www-data:www-data /var/www/html/
+chmod -R 755 /var/www/html/
+mkdir /var/www/html/wp-content/uploads
+chown -R www-data:www-data /var/www/html/wp-content/uploads/
 echo "<?php echo phpinfo();?>" > /var/www/html/info.php
+
+cd /var/www/html
+
+# Admin user
+wp core install --url=$DOMAIN_NAME --title=Inception --admin_name=$MYSQL_ADMIN --admin_password=$MYSQL_ADMIN_PASSWORD --admin_email=$MYSQL_ADMIN@gmail.com --allow-root
+
+# Installing redis plugin
+wp config set WP_REDIS_HOST redis --allow-root
+wp config set WP_REDIS_PORT 6379 --raw --allow-root
+wp config set WP_REDIS_DATABASE 0 --raw --allow-root
+wp config set WP_REDIS_PREFIX $DOMAIN_NAME --allow-root
+wp config set WP_REDIS_TIMEOUT 1 --raw --allow-root
+wp config set WP_REDIS_READ_TIMEOUT 1 --raw --allow-root
+wp plugin install redis-cache --allow-root
+wp plugin update --all --allow-root
+wp plugin activate redis-cache --allow-root
+wp redis enable --allow-root
+
 echo "Wordpress sh script done"
 mkdir -p /run/php
 /usr/sbin/php-fpm7.3 -F -R
